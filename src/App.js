69bfd03ee80b6ec1c94f40f7f5a7c85f402bf152
VyTsan <tosanvy@gmail.com>
2022-07-02 21:29:49 +0700

    Added some CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Ex1 from './Ex1/Ex1';
+// import Ex1 from './Ex1/Ex1';
 import Ex2 from './Ex2/Ex2';
 
 function App() {
   return (
     <div className="App">
-      <h1>BÀI TẬP SỐ 4</h1>
+      {/* <h1>BÀI TẬP SỐ 4</h1>
  
       <h2>Bài 4.1</h2>
       <Ex1 />
       <br/>
-      <h2>Bài 4.2</h2>
+      <h2>Bài 4.2</h2> */}
       <Ex2 />
     </div>
   );
--- src/Ex2/Ex2.jsx
+++ src/Ex2/Ex2.jsx
@@ -2,43 +2,47 @@ import React from 'react'
 import Task from '../components/Task/Task'
 import './ex2.css'
 import { useState } from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Ex2 = () => {
   const [tasks, setTasks] = useState([])
   function removeContent (i) {
     setTasks([...tasks].filter((_,index) => index !== i))
+    toast.success('Deleted')
   }
   const [input, setInput] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input === '')  {
-      alert('You added an empty task!');
+      toast.error('Task should not be empty!')
       return;
     }
     setTasks([...tasks,input]);
     setInput('');
-    console.log(tasks);
   }
   
 
   return (
     <div style={{fontFamily: 'cursive'}}>
-        <h1>Todo</h1>
-        <form onSubmit={handleSubmit}>
+        <h1>Todo List</h1>
+
+        <form onSubmit={handleSubmit} className='form'>
             <input 
-            type='text'
-            placeholder='Add a task...'
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
+              type='text'
+              placeholder='Add a task...'
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
             />
-            <button>Add</button>
+            <button className='btn btn-primary'>Add</button>
         </form>
+ 
         <div className='box'>
           {tasks.map((task,index) => (
               <Task key={index} content={task} i={index} removeContent={removeContent}/>
           ))}
         </div>
+        <ToastContainer/>
     </div>
   )
 }
